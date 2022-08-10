a4ad17207d4a2a0210756d8760d20ad14bb4c40f
Amitt Dosanjh <amitt.dosanjh@gmail.com>
2022-08-10 11:08:02 -0500

    Added functionality to get repos from database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import axios from "axios";
 import * as React from "react";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import AddRepoForm from "./components/AddRepoForm";
 
 const App: FC = () => {
   const [pageView, setPageView] = useState("Home");
+
+  useEffect(() => {
+    axios
+      .get("/repo")
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
